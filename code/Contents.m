02937d266fb4bca7b5da7d6a614d6c394c30af36
Create new release by ehennestad <>
2025-04-14 18:11:20 +0000

    Final checkins for release v0.9.10

--- code/Contents.m
+++ code/Contents.m
@@ -1,5 +1,5 @@
 % openMINDS Metadata Model
-% Version 0.9.7 (R2022b+) 14-Apr-2025
+% Version 0.9.10 (R2022b+) 14-Apr-2025
 %
 % Copyright (c) 2025, openMetadataInitiative
 % ------------------------------------------
